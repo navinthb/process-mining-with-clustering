ee88c077fec7635a9d970e6370140472f15df5bb
Navinth Bakmeedeniya <navinthb@gmail.com>
2024-04-12 21:21:55 +0530

    Conformance Checking for clusters against full log
    Code cleanup for Process Discovery

---
+++ FrequentFlowVariantsWithHierarchicalClustering/Main/ConformanceChecking/ConformanceCheckingHandler.py
@@ -0,0 +1,20 @@
+import pm4py
+import pandas as pd
+from pm4py.objects.conversion.log import converter as log_converter
+
+
+# Perform conformance check by using token based replay method
+def get_conformance(full_process_model, cluster_event_log, is_event_log):
+
+    # Convert the Data Frame into an EventLog, if not
+    if not is_event_log:
+        cluster_event_log = log_converter.apply(cluster_event_log, variant = log_converter.Variants.TO_EVENT_LOG)
+    # GEnerate a petrinet from the full proces model
+    petri_net, initial_marking_, final_marking = pm4py.convert_to_petri_net(full_process_model)
+
+    # CAlcualte finess & precision values of the cluster event log against full process model 
+    # using the token based replay method 
+    fitness = pm4py.fitness_token_based_replay(cluster_event_log, petri_net, initial_marking_, final_marking)
+    precision = pm4py.precision_token_based_replay(cluster_event_log, petri_net, initial_marking_, final_marking)
+
+    return fitness, precision
--- FrequentFlowVariantsWithHierarchicalClustering/Main/ProcessDiscovery/ProcessDiscoveryHandler.py
+++ FrequentFlowVariantsWithHierarchicalClustering/Main/ProcessDiscovery/ProcessDiscoveryHandler.py
@@ -10,36 +10,41 @@ def create_cluster_full_event_log(cluster, trace_event_flows_df, full_log):
     cluster_strings = [str(tup) for tup in cluster]
     cluster_df = pd.DataFrame(cluster_strings, columns=['Event Flow'])
 
-    #cluster1_event_log_df = pd.read_csv("Data\\Processed\\Cluster1_Event_Log.csv")
-
-    # join cluster_df with trace_event_flows_df to get the corresponding actual concept:name of the trace
+    # Join cluster_df with trace_event_flows_df to get the corresponding actual concept:name of the trace
     # as it is in the full event log
     cluster_event_log_actual_df = pd.merge(cluster_df, trace_event_flows_df, on='Event Flow', how='inner')
     #cluster_event_log_actual_df
     # Covert full log (in pm4py.objects.log.obj.EventLog type) into a dataframe
     full_log = log_converter.apply(full_log, variant=log_converter.Variants.TO_DATA_FRAME)
 
-    # join cluster_event_log_actual_df with the full event log based on case concept:name
+    # Join cluster_event_log_actual_df with the full event log based on case concept:name
     # to find the corresponding actual trace records for the cluster
     cluster_event_log_actual_df = pd.merge(cluster_event_log_actual_df, full_log, left_on='Case ID', right_on='case:concept:name', how='inner')
 
-    # drop the additional supportive columns from the cluster event log 
+    # Drop the additional supportive columns from the cluster event log 
     cluster_event_log_actual_df = cluster_event_log_actual_df.drop(columns=['Event Flow', 'Case ID'])
 
     return cluster_event_log_actual_df
 
 
+def discover_process(event_log, is_event_log):
+
+    # Convert the Data Frame into an EventLog if not
+    if not is_event_log:
+        event_log = log_converter.apply(event_log, variant = log_converter.Variants.TO_EVENT_LOG)
+
+    # Discover the process using Heuristic miner
+    process_model = heuristics_miner.apply_heu(event_log, parameters={heuristics_miner.Variants.CLASSIC.value.Parameters.DEPENDENCY_THRESH: 0.5})
+
+    return process_model
+
     
-def discover_process(event_log_df, save_file):
+def visualize_process(process_model, save_file):
 
-    # Convert the Data Frame into an EventLog
-    event_log = log_converter.apply(event_log_df, variant = log_converter.Variants.TO_EVENT_LOG)
-
-    # Discover the process using Heuristic miner & visualize
-    heu_net = heuristics_miner.apply_heu(event_log, parameters={heuristics_miner.Variants.CLASSIC.value.Parameters.DEPENDENCY_THRESH: 0.5})
-    gviz = hn_visualizer.apply(heu_net)
+    # Visualize the process
+    gviz = hn_visualizer.apply(process_model)
     hn_visualizer.view(gviz)
 
-    #save file to disk
+    # Save file to disk
     if save_file is not None:
         hn_visualizer.save(gviz, save_file)
